e3177c74a791335d34950d6483c80c4e1d9de46e
Maxim Shafarostov <capestiff@ya.ru>
2017-08-11 09:11:11 +0300

    first solution

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Password Strength Calculator
 
-[TODO. There will be project description]
+The program asks user for the password, evaluate its strength and prints the score.
+
+# How to start on Linux/Windows
+
+```
+$ python password_strength.py  # maybe you should use python3 instead of python due to your system dependencies
+Enter your password (6 characters or more): 10021984
+Your password has score: 4 of 10
+```
 
 # Project Goals
 
--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,60 @@
+import re
+
+
+def get_password_blacklist():
+    # I've got this password_blacklist from:
+    # https://github.com/danielmiessler/SecLists/blob/master/Passwords/500-worst-passwords.txt
+    with open('500-worst-passwords.txt', 'r') as file:
+        password_blacklist = [line.strip() for line in file]
+
+    return password_blacklist
 
 
 def get_password_strength(password):
-    pass
+    evaluation_point = 0
+
+    # evaluating for length
+    if len(password) in range(8, 16):
+        evaluation_point += 1
+    elif len(password) > 16:
+        evaluation_point += 2
+
+    # evaluating for lowercase
+    if re.search('[a-z]', password):
+        evaluation_point += 1
+
+    # evaluating for uppercase
+    if re.search('[A-Z]', password):
+        evaluation_point += 1
+
+    # evaluating for digits
+    if re.search('[0-9]', password):
+        evaluation_point += 1
+
+    # evaluating for non-alphanumeric symbols
+    if re.search('\W+', password):
+        evaluation_point += 1
+
+    # evaluating for special characters @, #, $, %
+    if re.search('[@#$%]', password):
+        evaluation_point += 1
+
+    # evaluating the password isn't in and hasn't words from the blacklist
+    password_blacklist = get_password_blacklist()
+    password_has_blacklist_word = False
+
+    for word in password_blacklist:
+        if word in password.lower():
+            password_has_blacklist_word = True
+
+    if password not in password_blacklist and not password_has_blacklist_word:
+        evaluation_point += 2
+
+    return evaluation_point
 
 
 if __name__ == '__main__':
-    pass
+    user_password = ''
+    while len(user_password) < 6:
+        user_password = input('Enter your password (6 characters or more): ')
+    print('Your password has score: {} of 10'.format(get_password_strength(user_password)))
